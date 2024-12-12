455bfaa1c22daa16017ab4cdeaf12894d1353fd7
Robson Alves <contato@robsonalves.dev.br>
2024-12-12 00:09:16 -0300

    configure ESLint and Prettier to handle end-of-line characters automatically

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    //'object-curly-spacing': ['error', 'never'], // Adicionado para remover espaços dentro das chaves
-    //'array-bracket-spacing': ['error', 'always'], // Adicionado para remover espaços dentro dos colchetes
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 };
